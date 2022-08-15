aa0958190d4266b605c1fabaa0d25b4ae41a53a4
Mahidy_Hasan <mahidyhasan89@gmail.com>
2022-08-15 12:36:14 -0700

    new info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import style from './App.module.css';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>React Deployment</h1>
+    </div>
   )
 }
